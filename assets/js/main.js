ec00d4c4638babd6e08a25798ff2602ac37cda1b
ayberkkk <ayberktiglii@gmail.com>
2023-07-14 15:41:55 +0300

    #5
    favorites fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,18 +122,11 @@ function createMovieCard(movie) {
   const favoriteButton = document.createElement("button");
   favoriteButton.classList.add("btn", "favorite-button");
 
-  if (isFavorite(movie)) {
-    favoriteButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
-  </svg>`;
-  } else {
-    favoriteButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16"><path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/></svg>`;
-  }
+  updateMovieCardFavoriteIcon(favoriteButton, movie);
 
   favoriteButton.addEventListener("click", () => {
     toggleFavorite(movie);
-    renderFavorites();
-    updateMovieCardFavoriteIcon(movieDiv, favoriteButton, movie);
+    updateMovieCardFavoriteIcon(favoriteButton, movie);
     favoriteButton.classList.add("animated", "bounce");
     setTimeout(() => {
       favoriteButton.classList.remove("animated", "bounce");
@@ -151,7 +144,7 @@ function isFavorite(movie) {
   return favorites.some((fav) => fav.id === movie.id);
 }
 
-function updateMovieCardFavoriteIcon(movieDiv, favoriteButton, movie) {
+function updateMovieCardFavoriteIcon(favoriteButton, movie) {
   const isMovieFavorite = isFavorite(movie);
 
   if (isMovieFavorite) {
@@ -162,12 +155,17 @@ function updateMovieCardFavoriteIcon(movieDiv, favoriteButton, movie) {
     favoriteButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16"><path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/></svg>`;
   }
 
-  // Kırmızı bir görsel geri bildirim için seçili filmin kartını vurgula
-  if (isMovieFavorite) {
-    movieDiv.classList.add("favorite");
-  } else {
-    movieDiv.classList.remove("favorite");
-  }
+  const movieDivs = document.getElementsByClassName("movie-card");
+  Array.from(movieDivs).forEach((movieDiv) => {
+    const movieId = movieDiv.querySelector("img").alt;
+    if (movieId === movie.title) {
+      if (isMovieFavorite) {
+        movieDiv.classList.add("favorite");
+      } else {
+        movieDiv.classList.remove("favorite");
+      }
+    }
+  });
 }
 
 function toggleFavorite(movie) {
@@ -237,7 +235,7 @@ function renderFavorites() {
       favoriteButton.addEventListener("click", () => {
         toggleFavorite(movie);
         renderFavorites();
-        updateMovieCardFavoriteIcon(movieDiv, favoriteButton, movie);
+        updateMovieCardFavoriteIcon(favoriteButton, movie);
         favoriteButton.classList.add("animated", "bounce");
         setTimeout(() => {
           favoriteButton.classList.remove("animated", "bounce");
@@ -290,7 +288,6 @@ searchButton.addEventListener("click", () => {
 
   if (searchTerm !== "") {
     searchMovies(searchTerm);
-
     const loadingElement = document.createElement("div");
     loadingElement.classList.add("loading");
     loadingElement.innerHTML =
